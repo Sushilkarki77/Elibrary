d629d6f64af3360a98d035f66ef2298c075a1dfa
Sushil <sushilkarki352@gmail.com>
2025-05-29 23:01:54 -0500

    action buttons

--- frontend/src/components/DocumentItem.tsx
+++ frontend/src/components/DocumentItem.tsx
@@ -19,7 +19,21 @@ const DocumentItem: React.FC<DocumentProps> = ({ document, onDeleteClick, onGene
             <td className="px-2 py-2 text-sm text-gray-800">{document.documentLabel}</td>
             <td className="px-2 py-2 text-sm text-gray-800">{formatDate(document.createdAt)}</td>
             <td className="px-2 py-2 text-sm text-gray-800 flex gap-2">
-                <ActionDropDown onGenerateSummary={() => onGenerateSummary(document._id)} onDisplayOriginalFile={() => onDisplayOriginalFile(document._id)} onGenerateQuiz={() => onGenerateQuiz(document._id)} onDeleteClick={() => onDeleteClick(document._id)} />
+                {/* <ActionDropDown onGenerateSummary={() => onGenerateSummary(document._id)} onDisplayOriginalFile={() => onDisplayOriginalFile(document._id)} onGenerateQuiz={() => onGenerateQuiz(document._id)} onDeleteClick={() => onDeleteClick(document._id)} /> */}
+
+                <button onClick={() => onDisplayOriginalFile(document._id)} className="px-2 py-1 text-xs border-2 border-blue-500 text-blue-500 rounded-md hover:bg-blue-500 hover:text-white transition duration-200">
+                    View
+                </button>
+                <button onClick={() => onGenerateSummary(document._id)} className="px-2 py-1 text-xs border-2 border-teal-600 text-teal-600 rounded-md hover:bg-teal-600 hover:text-white transition duration-200">
+                    Summary
+                </button>
+                <button onClick={() => onGenerateQuiz(document._id)} className="px-2 py-1 text-xs border-2 border-green-500 text-green-500 rounded-md hover:bg-green-500 hover:text-white transition duration-200">
+                    Quiz
+                </button>
+
+                <button onClick={() => onDeleteClick(document._id)} className="px-2 py-1 text-xs border-2 border-red-500 text-red-500 rounded-md hover:bg-red-500 hover:text-white transition duration-200">
+                    Delete
+                </button>
             </td>
         </tr>
     )
