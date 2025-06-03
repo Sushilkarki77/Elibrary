14ac5ead379306df98803cac4d2c63c528c4d2e1
Sushil <sushilkarki352@gmail.com>
2025-06-02 22:22:09 -0500

    Ui improvement

--- frontend/src/components/DocumentItem.tsx
+++ frontend/src/components/DocumentItem.tsx
@@ -21,7 +21,7 @@ const DocumentItem: React.FC<DocumentProps> = ({ document, onDeleteClick, onGene
             <td className="px-2 py-2 text-sm text-gray-800 flex gap-2">
                 {/* <ActionDropDown onGenerateSummary={() => onGenerateSummary(document._id)} onDisplayOriginalFile={() => onDisplayOriginalFile(document._id)} onGenerateQuiz={() => onGenerateQuiz(document._id)} onDeleteClick={() => onDeleteClick(document._id)} /> */}
 
-                <button onClick={() => onDisplayOriginalFile(document._id)} className="px-2 py-1 text-xs border-2 border-blue-500 text-blue-500 rounded-md hover:bg-blue-500 hover:text-white transition duration-200">
+                {/* <button onClick={() => onDisplayOriginalFile(document._id)} className="px-2 py-1 text-xs border-2 border-blue-500 text-blue-500 rounded-md hover:bg-blue-500 hover:text-white transition duration-200">
                     View
                 </button>
                 <button onClick={() => onGenerateSummary(document._id)} className="px-2 py-1 text-xs border-2 border-teal-600 text-teal-600 rounded-md hover:bg-teal-600 hover:text-white transition duration-200">
@@ -33,7 +33,20 @@ const DocumentItem: React.FC<DocumentProps> = ({ document, onDeleteClick, onGene
 
                 <button onClick={() => onDeleteClick(document._id)} className="px-2 py-1 text-xs border-2 border-red-500 text-red-500 rounded-md hover:bg-red-500 hover:text-white transition duration-200">
                     Delete
+                </button> */}
+                <button onClick={() => onDisplayOriginalFile(document._id)} className="px-2 py-1 text-xs text-blue-600  hover:text-blue-800 transition duration-200">
+                    View
                 </button>
+                <button onClick={() => onGenerateSummary(document._id)} className="px-2 py-1 text-xs text-teal-600  hover:text-teal-800 transition duration-200">
+                    Summary
+                </button>
+                <button onClick={() => onGenerateQuiz(document._id)} className="px-2 py-1 text-xs text-green-600  hover:text-green-800 transition duration-200">
+                    Quiz
+                </button>
+                <button onClick={() => onDeleteClick(document._id)} className="px-2 py-1 text-xs text-red-600  hover:text-red-800 transition duration-200">
+                    Delete
+                </button>
+
             </td>
         </tr>
     )
